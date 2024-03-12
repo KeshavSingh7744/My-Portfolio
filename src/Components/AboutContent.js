b26e2ca065323d6eb5ccdcacf130ad1987e3e149
KEshav <keshavsingh7744@gmail.com>
2024-03-12 16:23:23 +0530

    changed animation duration

--- src/Components/AboutContent.js
+++ src/Components/AboutContent.js
@@ -86,7 +86,7 @@ const AboutContent = () => {
 
       <div className="text-white w-full h-full flex items-center justify-between px-8">
         <div className="info">
-          <div className="glass-card card1 w-[400px] h-[300px]  duration-200 hover:bg-transparent">
+          <div className="glass-card card1 w-[400px] h-[300px]  duration-500 hover:bg-transparent">
             <p className="leading-8 text-lg cursor-default para-about ">
               <span className="text-gray-400 font-bold text-[22px]">
                 Hi there!
@@ -107,7 +107,7 @@ const AboutContent = () => {
         </div>
 
         <div className="skills">
-          <div className="glass-card card2 w-[402px] h-[300px] duration-200 bg-transparent">
+          <div className="glass-card card2 w-[402px] h-[300px] duration-500 bg-transparent">
             <h1 className="text-center h1 cursor-default text-cyan-400 -mt-1">
               SKILLS
             </h1>
